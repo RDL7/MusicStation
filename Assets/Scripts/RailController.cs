c6367c9f5326896f407abc4722254aafec2aa6fd
Eric Dou <lil.reenas@gmail.com>
2020-02-01 06:17:39 +0200

    hz

---
+++ Assets/Scripts/RailController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailController : MonoBehaviour
+{
+
+    public GameObject stick;
+    public bool canShowStick = true;
+    public bool doOnce = false;
+    // Start is called before the first frame update
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        if (canShowStick)
+        {
+
+            stick.SetActive (canShowStick);
+        }
+        else
+        {
+            stick.SetActive (canShowStick);
+        }
+    }
+
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.tag == "Back Trigger")
+        {
+            EventManager.em.RailsLeave ();
+        }
+
+        if (!canShowStick)
+        {
+            if (other.tag == "Front Trigger")
+            {
+                Debug.Log (transform.name);
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
