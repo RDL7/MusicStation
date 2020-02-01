57f6ac49586d7a0e1c5619820590a5406da7d514
Rolands <rolands.cipa@gmail.com>
2020-02-01 23:35:16 +0200

    added input cache list

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -19,6 +19,7 @@ public class InputManager : MonoBehaviour
     public AudioClip Si;
 
     public GameObject GameUI;
+    List<int> PressedKeys = new List<int>();
 
     //red, oran, dzelt, zals, sky blue, zils, violets,
     void NoteOn(MidiChannel channel, int note, float velocity)
@@ -34,33 +35,41 @@ public class InputManager : MonoBehaviour
             case (int)NoteColors.Red:
                 print("Red");
                 audioSource.PlayOneShot(Do);
+                PressedKeys.Add((int)NoteColors.Red);
                 break;
             case (int)NoteColors.Orange:
                 print("Orange");
                 audioSource.PlayOneShot(Re);
+                PressedKeys.Add((int)NoteColors.Orange);
                 break;
             case (int)NoteColors.Yellow:
                 print("Yellow");
                 audioSource.PlayOneShot(Mi);
+                PressedKeys.Add((int)NoteColors.Yellow);
                 break;
             case (int)NoteColors.Green:
                 print("Green");
                 audioSource.PlayOneShot(Fa);
+                PressedKeys.Add((int)NoteColors.Green);
                 break;
             case (int)NoteColors.SkyBlue:
                 print("SkyBlue");
                 audioSource.PlayOneShot(Sol);
+                PressedKeys.Add((int)NoteColors.SkyBlue);
                 break;
             case (int)NoteColors.Blue:
                 print("Blue");
                 audioSource.PlayOneShot(La);
+                PressedKeys.Add((int)NoteColors.Blue);
                 break;
             case (int)NoteColors.purple:
                 print("purple");
                 audioSource.PlayOneShot(Si);
+                PressedKeys.Add((int)NoteColors.purple);
                 break;
             default:
                 print("No Enum");
+                PressedKeys.Add(-1);//for noot views value, for not usefull
                 break;
         }
     }
